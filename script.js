e72a42e99fb0a693fdf1215545a6ee3af37ec0a0
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-13 16:59:38 +0100

    aggiunto pulsante annulla e funzione di reset degli input/html

--- script.js
+++ script.js
@@ -20,26 +20,46 @@ document.getElementById('genera').addEventListener('click',
     var costoBiglietto = km * 0.21;
     console.log(costoBiglietto);
     var sconto;
-    // sconto minorenni
+    // sconti
     if (eta == 'minorenne') {
+      // sconto minorenni
       sconto = (costoBiglietto * 20) / 100;
       console.log(sconto);
       costoBiglietto += -sconto;
       console.log(costoBiglietto);
-      // sconto over 65
+
     } else if (eta == 'over 65') {
+      // sconto over 65
       sconto = (costoBiglietto * 40) / 100;
       console.log(sconto);
       costoBiglietto = costoBiglietto - sconto;
       console.log(costoBiglietto);
-    } else {
 
+    } else {
+      // maggiorenni
     }
+    // numero carrozza
     var carrozza = Math.floor(Math.random() * 9) + 1;
+    // numero treno
     var codiceCp = Math.floor(Math.random() * 100000) + 90000;
+    // assegnazione valori all'HTML
     document.getElementById('passeggero').innerHTML = nomeCognome;
     document.getElementById('offerta').innerHTML = eta;
     document.getElementById('carrozza').innerHTML = carrozza;
     document.getElementById('codice_cp').innerHTML = codiceCp;
     document.getElementById('costo_biglietto').innerHTML = costoBiglietto;
-})
+  }
+)
+// reset input
+document.getElementById('annulla').addEventListener('click',
+  function() {
+    document.getElementById('nome_cognome').value = '';
+    document.getElementById('km').value = '';
+    document.getElementById('eta').value = '';
+    document.getElementById('passeggero').innerHTML = '';
+    document.getElementById('offerta').innerHTML = '';
+    document.getElementById('carrozza').innerHTML = '';
+    document.getElementById('codice_cp').innerHTML = '';
+    document.getElementById('costo_biglietto').innerHTML = '';
+  }
+)
